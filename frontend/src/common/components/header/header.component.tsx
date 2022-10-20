ea14f7070bea541b53cc522f1542a63378ac1a7d
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-10-20 12:01:52 +0300

    storybook, tailwind, craco, header

---
+++ frontend/src/common/components/header/header.component.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <div className="h-[48px] shadow-xl px-6">
+      <div className="flex h-full items-center">
+        <a href="/" className="text-xl font-semibold">
+          🍕 pizzy
+        </a>
+      </div>
+    </div>
+  );
+};
